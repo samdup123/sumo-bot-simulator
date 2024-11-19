3f9317d5d51e27fa32f6a321a825e418f293bc23
Samuel DuPlessis <samdup123@gmail.com>
2024-11-19 17:58:31 -0500

    initial world set up

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 local inspect = require('inspect')
 
 function love.conf(t)
-    t.title = "Sumo Bot Simulator"
-    t.author = "Sam DuPlessis"
-    t.version = "11.3"
+  t.title = "Sumo Bot Simulator"
+  t.author = "Sam DuPlessis"
+  t.version = "11.3"
 end
 
 function love.load()
@@ -12,10 +12,20 @@ function love.load()
   objects = {} -- table to hold all our physical objects
 
   --let's create the ground
-  objects.black_circle = {}
-  objects.black_circle.body = love.physics.newBody(world, 200, 200)
-  objects.black_circle.shape = love.physics.newCircleShape(80)
-  objects.black_circle.fixture = love.physics.newFixture(objects.black_circle.body, objects.black_circle.shape)
+  objects.robot_body = {}
+  objects.robot_body.body = love.physics.newBody(world, .5, .5)
+  objects.robot_body.shape = love.physics.newRectangleShape(.1, .1)
+  objects.robot_body.fixture = love.physics.newFixture(objects.robot_body.body, objects.robot_body.shape)
+
+  objects.black_board = {}
+  objects.black_board.body = love.physics.newBody(world, .5, .5)
+  objects.black_board.shape = love.physics.newCircleShape(.85)
+  objects.black_board.fixture = love.physics.newFixture(objects.black_board.body, objects.black_board.shape)
+
+  objects.white_border = {}
+  objects.white_border.body = love.physics.newBody(world, .5, .5)
+  objects.white_border.shape = love.physics.newCircleShape(.95)
+  objects.white_border.fixture = love.physics.newFixture(objects.white_border.body, objects.black_board.shape)
 
   -- --let's create a ball
   -- objects.ball = {}
@@ -36,7 +46,7 @@ function love.load()
   -- objects.block2.fixture = love.physics.newFixture(objects.block2.body, objects.block2.shape, 2)
 
   --initial graphics setup
-  love.graphics.setBackgroundColor(0.41, 0.53, 0.97) --set the background color to a nice blue
+  love.graphics.setBackgroundColor(.1, .3, .8)
   love.window.setMode(650, 650) --set the window dimensions to 650 by 650 with no fullscreen, vsync on, and no antialiasing
 end
 
@@ -44,8 +54,28 @@ function love.update(dt)
   world:update(dt) --this puts the world into motion
 end
 
+local function rectangle_points(window_width, window_height, x1, y1, x2, y2, x3, y3, x4, y4)
+  local w = math.min(window_width, window_height)
+  return x1 * w, y1 * w, x2 * w, y2 * w, x3 * w, y3 * w, x4 * w, y4 * w
+end
+
+local function circle_points(window_width, window_height, x, y, r)
+  local w = math.min(window_width, window_height)
+  return x * w, y * w, r * w/2
+end
+
 function love.draw()
-  love.graphics.setColor(0.28, 0.63, 0.05)
-  love.graphics.circle("fill", objects.black_circle.body:getX(), objects.black_circle.body:getY(), objects.black_circle.shape:getRadius())
-  love.graphics.setColor(0.76, 0.18, 0.05)
+  window_width, window_height = love.graphics.getDimensions( )
+
+  love.graphics.setColor(1,1,1)
+  love.graphics.circle("fill", circle_points(window_width, window_height, objects.white_border.body:getX(),
+    objects.white_border.body:getY(), objects.white_border.shape:getRadius()))
+
+  love.graphics.setColor(0,0,0)
+  love.graphics.circle("fill", circle_points(window_width, window_height, objects.black_board.body:getX(),
+    objects.black_board.body:getY(), objects.black_board.shape:getRadius()))
+
+  love.graphics.setColor(.6,0,0)
+  love.graphics.polygon("fill", rectangle_points(window_width, window_height, objects.robot_body.body:getWorldPoints(objects.robot_body.shape:getPoints())))
+
 end
